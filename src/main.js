16ef8e57563d6f8a0038bf0b8e2926af079d414f
Maryna <marin.naumch@gmail.com>
2025-02-03 02:26:49 +0200

    try to found prob

---
+++ src/main.js
@@ -0,0 +1,44 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import { fetchImages } from "./js/pixabay-api.js";
+
+export const refs = {
+  form: document.querySelector(".form"),
+  gallery: document.querySelector(".gallery"),
+  loader: document.querySelector(".loader"),
+};
+
+refs.form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const inputValue = form.elements.state.value.trim();
+
+  refs.gallery.innerHTML = "";
+
+  if (!inputValue) {
+    iziToast.error({
+      message: "Please enter your request",
+      position: "topRight",
+    });
+    return;
+  }
+
+  refs.loader.classList.remove("is-hidden");
+
+  fetchImages(inputValue)
+    .catch(error => {
+      refs.loader.style.display = "none";
+      iziToast.error({
+        message: "No images found. Try another query!",
+          position: "topRight",
+      });
+      console.error(error);
+    })
+    .finally(() => {
+      refs.loader.classList.add("is-hidden");
+    });
+
+  refs.form.reset();
+}
